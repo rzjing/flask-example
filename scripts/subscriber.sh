6c51bf060430d5d794068ff3198583b05c787e67
rzjing <wangxiaoqiange@gmail.com>
2020-01-06 15:41:51 +0800

    小细节调整

--- scripts/subscriber.sh
+++ scripts/subscriber.sh
@@ -2,5 +2,7 @@
 
 cd /data/flask-example
 
+[[ ! -e 'logs' ]] && mkdir logs
+
 source scripts/env_dev.sh
 source venv/bin/activate && pip install -r requirements.txt && python app/common/subscriber.py
