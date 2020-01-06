a251eb20eab0da527fb2ad5c38d89912eb7b626b
rzjing <wangxiaoqiange@gmail.com>
2020-01-06 15:21:10 +0800

    debug

---
+++ scripts/env_dev.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export PYTHONPATH=`pwd`
+export ENV=dev
+export SERVER_PORT=5000
+export FLASK_DEBUG=True
--- scripts/subscriber.sh
+++ scripts/subscriber.sh
@@ -2,5 +2,5 @@
 
 cd /data/flask-example
 
-source env/dev.sh
+source scripts/env_dev.sh
 source venv/bin/activate && pip install -r requirements.txt && python app/common/subscriber.py
